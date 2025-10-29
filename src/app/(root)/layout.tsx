e13b9a199f83d87eb91bc1ac43bbe27b8f571f23
ChenMyae KayKhaing <112561446+Chen-Myae2K@users.noreply.github.com>
2025-10-29 16:54:23 +0630

    hero section done

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
 
-const layout = ({children} : {children: React.ReactNode}) => {
-  return (
-    <main className="font-work">
-
-    </main>
-  )
+const layout = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <main className="font-work-sans">
+            <Navbar />
+            {children}
+        </main>
+    )
 }
 
 export default layout
--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,9 +1,17 @@
 import Image from "next/image";
+import Navbar from "../components/Navbar";
+
+
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <section className="pink_container  ">
+      <h1 className="text-2xl heading">Pitch Your Startup,<br />Connect With Entrepreneurs</h1>
+      <p className="sub-heading max-w-3xl!">
+        Submit Ideas, Vote on Pitches, and Get Noticed in VIrtual Competitions.
+      </p>
+
+
+    </section>
   );
 }
