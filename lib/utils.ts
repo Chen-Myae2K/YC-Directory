088e568d2a193fcecc6b3761b021bf385c2ede69
ChenMyae KayKhaing <112561446+Chen-Myae2K@users.noreply.github.com>
2025-11-17 15:42:46 +0630

    startup form halfway

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,16 +1,20 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
 export function formatDate(dateString: string) {
-  const date = new Date(dateString)
+  const date = new Date(dateString);
   const options: Intl.DateTimeFormatOptions = {
     day: "numeric",
     month: "long",
     year: "numeric",
-  }
-  return date.toLocaleDateString("en-US", options)
+  };
+  return date.toLocaleDateString("en-US", options);
+}
+
+export function parseServerActionResponse<T>(response: T) {
+  return JSON.parse(JSON.stringify(response));
 }
